6332010df3e86284557dda7ff5e9afc53ca8b528
jlinburg <jlinbu@gmail.com>
2019-03-27 12:21:44 -0400

    2.1.4

---
+++ app/src/main/java/com/linburgtech/collegeapp_linburg2019/Guardian.java
@@ -0,0 +1,25 @@
+package com.linburgtech.collegeapp_linburg2019;
+
+public class Guardian {
+
+    String lastName;
+    String firstName;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+
+}
